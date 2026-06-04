14d9d03bf8b7a9213c1974759f5e11dab5cb5533
Warren0721 <wb13161108576@gmail.com>
2026-06-04 00:43:00 -0400

    a1p2

--- a1p2/main.cpp
+++ a1p2/main.cpp
@@ -2,30 +2,43 @@
 #include<cstdio>
 
 using namespace std;
-
-int main(){
-    double number;
+double number;
     char units;
     double number2;
     char symbol;
+
+int main(){
+
     printf("This program converts Temperatures from Fahrenheit to Celsius and vice versa.\n");
+    do{
+            double number = 0.0;
+    double number2 = 0.0;
+    char units = 'O';     // 随便一个合理的默认字符
+
     printf("Please enter your temperature:");
-    scanf("%lf", &number);
+    scanf("%lf",  &number );
 
-    printf("Please enter the units (F/C):");
-    scanf("%c", &units);
+    printf("Please enter the units (F/C): ");
+    scanf(" %c",  &units );
 
-    if ( units=="F"){
-        &number2= (&number �C 32 ) * 5 / 9;
-        printf("A temperature of %d degrees Fahrenheit is equivalent to %d degrees Celsius.", number, number2);
-    else if (&units=="C"){
-        &number2 = (&number * 9 / 5) + 32
-        printf("A temperature of %d degrees Celsius is equivalent to %d degrees Fahrenheit.", number,number2 );}}
-    printf("Do you want another conversion? (Y/N):");
-    scanf("%c", &symbol);
-    if(&symbol==Y){
-            return 0;
+    if (units =='F'){
+        number2= (number -32 ) * 5 / 9;
+        printf("A temperature of %.2f degrees Fahrenheit is equivalent to %.2f degrees Celsius. \n", number, number2);
+        printf("Do you want another conversion? (Y/N):");
+        }
+    else if (units=='C'){
+        number2 = (number * 9 / 5) + 32;
+        printf("A temperature of %.2f degrees Celsius is equivalent to %.2f degrees Fahrenheit.", number,number2 );
+        printf("Do you want another conversion? (Y/N):");
+        }
+
+        scanf(" %c", &symbol);
+        } while ( symbol == 'Y');
+
+        if( symbol=='N'){
+            printf("Thank you. Goodbye.");
+                    return 0;}
+
 
     }
 
-}

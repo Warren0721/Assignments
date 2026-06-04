3285a29754b25769742a624b46225745705b1b06
Warren0721 <wb13161108576@gmail.com>
2026-06-04 01:14:53 -0400

    a1 p2 format fix

--- a1p2/main.cpp
+++ a1p2/main.cpp
@@ -10,33 +10,38 @@ double number;
 int main(){
 
     printf("This program converts Temperatures from Fahrenheit to Celsius and vice versa.\n");
+    int first = 0;
     do{
             double number = 0.0;
     double number2 = 0.0;
-    char units = 'O';     // 随便一个合理的默认字符
-
+    char units = 'O';
+    if (first>0) {
+            printf("\n");
+        }
     printf("Please enter your temperature:");
     scanf("%lf",  &number );
 
     printf("Please enter the units (F/C): ");
     scanf(" %c",  &units );
-
+    printf("\n");
     if (units =='F'){
         number2= (number -32 ) * 5 / 9;
         printf("A temperature of %.2f degrees Fahrenheit is equivalent to %.2f degrees Celsius. \n", number, number2);
         printf("Do you want another conversion? (Y/N):");
+        first++;
         }
     else if (units=='C'){
         number2 = (number * 9 / 5) + 32;
-        printf("A temperature of %.2f degrees Celsius is equivalent to %.2f degrees Fahrenheit.", number,number2 );
+        printf("A temperature of %.2f degrees Celsius is equivalent to %.2f degrees Fahrenheit.\n", number,number2 );
         printf("Do you want another conversion? (Y/N):");
+        first++;
         }
 
         scanf(" %c", &symbol);
-        } while ( symbol == 'Y');
+        } while ( symbol == 'Y') ;
 
         if( symbol=='N'){
-            printf("Thank you. Goodbye.");
+            printf("\nThank you. Goodbye.");
                     return 0;}
 
 

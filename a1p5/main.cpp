bdc1132f03338effe95df58c2470a770d588701b
Warren0721 <wb13161108576@gmail.com>
2026-06-04 15:31:59 -0400

    a1 p5 format fix

--- a1p5/main.cpp
+++ a1p5/main.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main()
 {
-    for (int n = 2; n <= 10000; ++n) {
-        for (int d = 2; d < n; ++d) {
-            if (n % d == 0) break;
-    }printf("%d",n);
-}
+    for (int n = 2; n <= 10000 ; ++n) {
+            char prime = true ;
+            for (int d = 2; d * d < n; ++d) {
+                    if (n % d == 0){
+                        prime = false ;
+                        break;
+                    }
+            }
+            if (prime == true){
+                    printf( " %d ", n );
+            }
+    }
 }
